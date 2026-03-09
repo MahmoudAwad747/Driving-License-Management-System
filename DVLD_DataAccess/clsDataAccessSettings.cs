b0a677729b1e74c1ebf1dbc14335660b446df526
Mahmoud Awad <mwd8244@gmail.com>
2026-03-10 02:57:58 +0300

    Update

--- DVLD_DataAccess/clsDataAccessSettings.cs
+++ DVLD_DataAccess/clsDataAccessSettings.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Configuration;
 
 namespace DVLD_DataAccess
 {
-    static class clsDataAccessSettings
+    internal static class clsDataAccessSettings
     {
-        public static string ConnectionString = "Server=.;Database=DVLD;User Id=sa;Password=123456;";
+        public static string ConnectionString = ConfigurationManager.ConnectionStrings["MyDBConnection"].ConnectionString;
     }
 }
